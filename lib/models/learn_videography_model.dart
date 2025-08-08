1bb16fd65c27081e0e632893524256872de33ce8
Steven Ho Yong <syong789@hotmail.com>
2025-08-08 11:36:41 +0800

    Videography Guides List & Details pages (#4)
    * Added Noto San Font & fixed Homepage card title text overflows
    
    * Added Orientation Awareness & Noto Sans font
    
    - The homepage layout now adjusts based on the device's dimensions/orientation
    - Changed the app's font to Noto Sans
    
    * Learning Guides Page
    
    * Finished Learning List & Equipment Guides List Pages' layouts
    
    * Search Bar functionality for Learning List & Equipment Guides pages
    
    * Improved routing path names
    
    * Added Filter fuction to search in Learning List & Eqyipmet Guides pages
    
    * Videography List & Detail pages

---
+++ lib/models/learn_videography_model.dart
@@ -0,0 +1,40 @@
+import 'package:yaml/yaml.dart';
+
+class VideographyGuide {
+  final String id;
+  final String name;
+  final List<String> covers;
+  final List<GuideSection> sections;
+
+  VideographyGuide({
+    required this.id,
+    required this.name,
+    required this.covers,
+    required this.sections,
+  });
+
+  factory VideographyGuide.fromYaml(YamlMap yaml) {
+    final secs = (yaml['sections'] as YamlList).map((s) {
+      final m = s as YamlMap;
+      return GuideSection(
+        title: m['title'] as String,
+        images: List<String>.from(m['images'] ?? []),
+        body: m['body'] as String,
+      );
+    }).toList();
+
+    return VideographyGuide(
+      id: yaml['id'] as String,
+      name: yaml['name'] as String,
+      covers: List<String>.from(yaml['covers'] ?? []),
+      sections: secs,
+    );
+  }
+}
+
+class GuideSection {
+  final String title;
+  final List<String> images;
+  final String body;
+  GuideSection({required this.title, required this.images, required this.body});
+}
--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
 import 'pages/learning_list_page.dart';
 import 'pages/learn_equip_list_page.dart';
+import 'pages/learn_videography_list_page.dart';
+import 'pages/learn_videography_detail_page.dart';
 import 'pages/learn_equip_detail_page.dart';
 import 'pages/scenario_list_page.dart';
 import 'pages/scenario_detail_page.dart';
@@ -18,9 +20,18 @@ final GoRouter appRouter = GoRouter(
       builder: (_, _) => const LearnEquipListPage(),
     ),
     GoRoute(
-      path: '/learn/equip-guides:id',
+      path: '/learn/equip-guides/:id',
       builder: (_, state) =>
           EquipDetailPage(itemId: state.pathParameters['id']!),
+    ),
+    GoRoute(
+      path: '/learn/videography-guides',
+      builder: (_, _) => const LearnVideographyListPage(),
+    ),
+    GoRoute(
+      path: '/learn/videography-guides/:id',
+      builder: (_, state) =>
+          VideographyDetailPage(itemID: state.pathParameters['id']!),
     ),
 
     GoRoute(path: '/scenarios', builder: (_, _) => const ScenarioListPage()),
